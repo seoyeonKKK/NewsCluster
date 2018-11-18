db619b8bf9b5d692ac73f52e575fcfab0bbc8114
seoyeonKKK <syeon970204@duksung.ac.kr>
2018-11-18 18:41:20 +0900

    서버 배포

--- cluster/app.js
+++ cluster/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use(helmet());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
